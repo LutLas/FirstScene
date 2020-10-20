cf03ab0da7cdbd05d3220c666e63d282e18b977e
Muyoba Lutlas <lutakachihota@gmail.com>
2020-10-20 08:30:56 +0200

    fragMentation

--- app/src/main/java/io/eresawsaltul/firstscene/SecondFragment.kt
+++ app/src/main/java/io/eresawsaltul/firstscene/SecondFragment.kt
@@ -24,8 +24,12 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        view.findViewById<Button>(R.id.button_secondBack).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+        view.findViewById<Button>(R.id.button_secondNext).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment)
         }
     }
 }
